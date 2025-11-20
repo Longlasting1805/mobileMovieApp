15a660337e84d292ba23e850413679ce59f64905
Longlasting1805 <longlasting1805@gmail.com>
2025-11-20 11:47:12 +0100

    environment setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to my Movie App Adebola Peniel.</Text>
-    </View>
+  <View className="flex-1 justify-center items-center text-blue-200">
+  <Text className=" text-2xl font-bold animate-bounce text-dark-200">
+    Tailwind Works!
+  </Text>
+</View>
+
+
   );
 }
