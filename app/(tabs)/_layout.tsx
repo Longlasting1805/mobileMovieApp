f00ae49cd4cb6bec72d1edb8c0f847236b90b3e9
Longlasting1805 <longlasting1805@gmail.com>
2025-11-25 18:07:05 +0100

    Home tab added with an icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from "expo-router"
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const _layout = () => {
     return (
@@ -9,18 +11,36 @@ const _layout = () => {
                 name='index'
                 options={{
                     title: 'Home',
-                    headerShown: false
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <>
+
+                            <ImageBackground
+                                source={images.highlight}
+                                className='flex flex-row w-full 
+                                flex-1 min-w-[112px] min-h-14 mt-4 
+                                justify-center items-center rounded-full overflow-hidden'
+                            >
+                                <Image source={icons.home}
+                                        tintColor="#151312"
+                                        className='size-5'/>
+                                        <Text className='text-secondary text-base 
+                                        font-semibold ml-2'>Home</Text>
+                            </ImageBackground>
+
+                        </>
+                    )
                 }}
             />
             <Tabs.Screen
-                name='search' 
-                options={{ 
+                name='search'
+                options={{
                     title: 'Search',
                     headerShown: false
                 }}
-            
+
             />
-              <Tabs.Screen
+            <Tabs.Screen
                 name='saved'
                 options={{
                     title: 'saved',
@@ -28,12 +48,12 @@ const _layout = () => {
                 }}
             />
             <Tabs.Screen
-                name='profile' 
-                options={{ 
+                name='profile'
+                options={{
                     title: 'profile',
                     headerShown: false
                 }}
-            
+
             />
 
         </Tabs>
