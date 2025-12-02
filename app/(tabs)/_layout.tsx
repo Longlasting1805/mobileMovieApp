d03da9b085d9a3b62c884fb65fd3f0359da7b7cd
Longlasting1805 <longlasting1805@gmail.com>
2025-12-02 10:54:25 +0100

    still styling the tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,20 +4,29 @@ import { Tabs } from "expo-router"
 import { images } from '@/constants/images'
 import { icons } from '@/constants/icons'
 
-const TabIcon = () => {
-    return (
-        <ImageBackground
-            source={images.highlight}
-            className='flex flex-row w-full 
+const TabIcon = ({ focused, icon, title }: any) => {
+    if (focused) {
+
+
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className='flex flex-row w-full 
                                 flex-1 min-w-[112px] min-h-14 mt-4 
                                 justify-center items-center rounded-full overflow-hidden'
-        >
-            <Image source={icons.home}
-                tintColor="#151312"
-                className='size-5' />
-            <Text className='text-secondary text-base 
-                                        font-semibold ml-2'>Home</Text>
-        </ImageBackground>
+            >
+                <Image source={icon}
+                    tintColor="#151312"
+                    className='size-5' />
+                <Text className='text-secondary text-base 
+                                        font-semibold ml-2'>{title}</Text>
+            </ImageBackground>
+        )
+    }
+    return (
+        <View className='size-full justify-center item-center mt-4 rounded-full'>
+            <Image source={icon} tintColor="#A8B5DB" className='size-5'/>
+        </View>
     )
 
 }
@@ -31,10 +40,10 @@ const _layout = () => {
                     title: 'Home',
                     headerShown: false,
                     tabBarIcon: ({ focused }) => (
-                        <TabIcon 
-                        focused={focused} 
-                        icon={icons.home} 
-                        title="Home"
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            title="Home"
                         />
                     )
                 }}
@@ -45,10 +54,10 @@ const _layout = () => {
                     title: 'Search',
                     headerShown: false,
                     tabBarIcon: ({ focused }) => (
-                        <TabIcon 
-                        focused={focused} 
-                        icon={icons.search} 
-                        title="Search"
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
                         />
                     )
                 }}
@@ -59,11 +68,11 @@ const _layout = () => {
                 options={{
                     title: 'saved',
                     headerShown: false,
-                      tabBarIcon: ({ focused }) => (
-                        <TabIcon 
-                        focused={focused} 
-                        icon={icons.save} 
-                        title="Saved"
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Saved"
                         />
                     )
                 }}
@@ -73,11 +82,11 @@ const _layout = () => {
                 options={{
                     title: 'profile',
                     headerShown: false,
-                      tabBarIcon: ({ focused }) => (
-                        <TabIcon 
-                        focused={focused} 
-                        icon={icons.person} 
-                        title="Profile"
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.person}
+                            title="Profile"
                         />
                     )
                 }}
