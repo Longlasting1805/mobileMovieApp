de3cc3778c38b6acde18dcf92034e686ccc81218
Longlasting1805 <longlasting1805@gmail.com>
2025-12-09 11:27:34 +0100

    services folder created

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2Y2MjlhZmE1NGVkYjUzZThjZjJkODhlNGE5ZWE5YSIsIm5iZiI6MTc2NDkyNjU2Mi4zNDA5OTk4LCJzdWIiOiI2OTMyYTQ2MmViMGI1MDRjMTg0NzY0OWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.tBuR2yAb1d_DMbfCqQPg4UQDPTXzns76HRdPxtnAtPA'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
