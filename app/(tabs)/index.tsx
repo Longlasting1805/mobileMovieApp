c6ab8a028da31b10ac5c5c7341649e9cb849c548
Longlasting1805 <longlasting1805@gmail.com>
2025-12-30 13:06:08 +0100

    movie images added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from 'reac
 import { useRouter } from 'expo-router';
 import useFetch from '@/services/useFetch';
 import { fetchMovies } from '@/services/api';
+import MovieCard from '@/components/MovieCard';
 
 export default function Index() {
   const router = useRouter()
@@ -55,7 +56,9 @@ export default function Index() {
               <FlatList
                 data={movies}
                 renderItem={({ item }) => (
-                  <Text className="text-white text-sm">{item.title}</Text>
+                 <MovieCard 
+                    { ...item}
+                 />
                   
                 )}
                 keyExtractor={(item) => item.id.toString()}
