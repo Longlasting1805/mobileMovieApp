a8193228db1bab7dcae53914fe17f72a7d18f56f
Longlasting1805 <longlasting1805@gmail.com>
2026-01-06 11:06:23 +0100

    search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -23,17 +23,15 @@ const Search = () => {
   }), false)
 
   useEffect(() => {
-    const func = async () => { 
+    const timeoutId = setTimeout(async () => { 
     if (searchQuery.trim()) {
       await loadMovies()
     } else {
       reset()
     }
-  } 
+  }, 500)
 
-  func()
-
-
+  return() => clearTimeout(timeoutId)
     
   }, [searchQuery])
 
@@ -85,6 +83,17 @@ return (
             )}
         </>
       }
+     ListEmptyComponent={
+      !loading && !error ? (
+        <View className='mt-10 px-5'>
+          <Text className='text-center text-gray-500'>
+            {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
+
+          </Text>
+
+        </View>
+      ) : null
+     }
     />
   </View>
 )
